437df696363b7998b75fd04bf53bf92120dd56d4
Karl Sigel <heykarl@gmail.com>
2021-10-29 10:27:43 -0700

    added test for / route, added / route, added test for /red route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,4 +3,15 @@ const app = require('../lib/app');
 
 describe('app routes', () => {
 
+  it('receive / route using GET', async() => {
+    const res = await request(app).get('/');
+    expect(res.text).toEqual('hi');
+  });
+    
+  it('receive /red route using GET', async() => {
+    const res = await request(app).get('/red');
+    expect(res.text).toEqual('hi');
+  });
+    
+
 });
